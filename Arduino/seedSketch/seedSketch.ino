91617cb5893c969bd9eec7a079baecb9d011e320
Ricardo Rodriguez <ricardo@linux-buvj.site>
2014-07-30 00:14:12 -0700

    * Added inner command parser * Added primitive lookup table

--- Arduino/seedSketch/seedSketch.ino
+++ Arduino/seedSketch/seedSketch.ino
@@ -29,7 +29,11 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define XPOS 0
 #define YPOS 1
 #define DELTAY 2
- 
+
+/*
+Initialize Seed function pointers
+*/
+
  
 #define LOGO16_GLCD_HEIGHT 16
 #define LOGO16_GLCD_WIDTH  16
@@ -44,8 +48,25 @@ Adafruit_SSD1306 display(OLED_RESET);
 //SoftwareSerial mySerial(11, 12); //RX,TX
  
 int i = 0;
-String inputString = "";         // a string to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
+String commandKey = "";         // a string to hold incoming data key
+String commandValue = "";         // a string to hold incoming data value
+boolean commandComplete = false;  // whether the string is complete
+boolean readingKey = true;
+
+
+/***********************************
+** VALUES FOR THIS SEED COMPONENT **
+************************************/
+const int MAP_SIZE = 1;
+const String keySet[MAP_SIZE] = {"led1"};
+const int values[MAP_SIZE] = {13};
+
+/*
+   v:{N} - vibrate for N seconds (ex. v:5 will make seed vibrate for 5 seconds)
+   b:{led1}={5} blink pin P five times (ex. b:led1:4)
+*/
+
+//***********************************
 
 void setup()   {
   // initialize serial:
@@ -64,38 +85,66 @@ void setup()   {
   // Clear the buffer.
   display.clearDisplay();
  
-  // reserve 200 bytes for the inputString:
-  inputString.reserve(200);
- 
-  // text display tests
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println("Awaiting input");
-  display.display();
+  // reserve 10 bytes for the inputString:
+  commandKey.reserve(10);
+  commandValue.reserve(10);
  
   // initialize serial:
   Serial.begin(9600);
-}
+  
+  pinMode(13, OUTPUT);
+  blink("led1");
+}  
  
 void loop() {
  
- if (stringComplete) {
-    printToScreen(inputString);
+ if (commandComplete) {
+    //printToScreen(commandKey + ":" + commandValue);
+    
+    if(containsNestedCommand(commandValue)) {
+      String nestedKey = getNestedKey(commandValue);
+      String nestedValue = getNestedValue(commandValue);
+      Serial.println(nestedKey);
+      Serial.println(nestedValue);
+    }
+    
     // clear the string:
-    inputString = "";
-    stringComplete = false;
+    commandKey = "";
+    commandValue = "";
+    commandComplete = false;
   }
   // if there's any serial available, read it:
   while (Serial.available()) {
     char c = Serial.read();
+    
     if (c == '\n') {
-      stringComplete = true;
-      Serial.println(inputString);
+      commandComplete = true;
+      readingKey = true;
+      Serial.print(commandKey);
+      Serial.print(":");
+      Serial.println(commandValue);
+    } else if (c == ':') {
+      readingKey = false;
     } else {
-      inputString += c;
-    }  
+      if(readingKey) {
+        commandKey += c;
+      } else {
+        commandValue += c;
+      }
+    }
   }
+}
+
+boolean containsNestedCommand(String command) {
+    return command.indexOf("=") != -1 && command.length() > 2;
+}
+
+String getNestedKey(String command) {
+    return command.substring(0, command.indexOf("="));
+}
+
+String getNestedValue(String command) {
+    return command.substring(command.indexOf("=") + 1);
 }
 
 void printToScreen(String inputString) {
@@ -111,4 +160,28 @@ void printToScreen(String inputString) {
 void printFreeMemory() {
   display.setCursor(0,15);
   display.println(freeMemory());
+}
+
+void blink(String led) {
+  int key = get(led);
+  if(key != -1) {
+    int ledPin = values[key];
+    Serial.println("Found pin for " + led + ": " + ledPin);
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+  }
+}
+
+int get(String key) {
+  for(int i = 0; i < MAP_SIZE; i++) {
+    if(keySet[i] == key) {
+      return i;
+    }
+  }
+  return -1;
 }
