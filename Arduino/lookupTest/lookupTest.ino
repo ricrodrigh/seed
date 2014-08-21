655e7b13e564d82b8d6499ffcf207b82b1394a99
Ricardo Rodriguez <rodrigur@amazon.com>
2014-08-20 22:57:02 -0700

    More changes

--- Arduino/lookupTest/lookupTest.ino
+++ Arduino/lookupTest/lookupTest.ino
@@ -5,78 +5,65 @@ Test the LookupTable functionality with memory
 consumption evaluation
 
 */
-#include <LookupTable.h>
+#include <HashMap.h>
 #include <MemoryFree.h>
-
 
 /***********************************
 ** VALUES FOR THIS SEED COMPONENT **
 ************************************/
-const int MAP_SIZE = 3;
-LookupTable table(MAP_SIZE);
+const int HASH_SIZE = 3;
 
-const String VALUE_A = "aValue";
-const String VALUE_B = "b";
-const String VALUE_C = "V";
-const String VALUE_D = "dVal";
+char VALUE_A[] = "aValue";
+char VALUE_B[] = "b";
+char VALUE_C[] = "V";
+
+//storage 
+HashType<char*,int> hashRawArray[HASH_SIZE]; 
+//handles the storage [search,retrieve,insert]
+HashMap<char*,int> hashMap = HashMap<char*,int>( hashRawArray , HASH_SIZE ); 
 
 void setup()   {
   // initialize serial:
   Serial.begin(9600);
   delay(2500);
   
-  testLookupTable();
-}
+  //setup hashmap
+  hashMap[0](VALUE_A, 180);
+  hashMap[1]("b", 198);
+  hashMap[2]("c", -1);
+  hashMap.debug();
   
-void testLookupTable () {
-  Serial.println("=Printall=");
-  printAll();
-  Serial.println("=modify=");
-  modify();
-  Serial.println("=print after modify=");
-  printAll();
-
-  Serial.println("=Get All value=");
-  getAllValues();
-} 
-
-void getAllValues() {
-  Serial.println("Index\t Value");
-  for(int i = 0; i < MAP_SIZE + 1; i++) {
-      getOneValue(i);
-  }
-}
-
-void getOneValue(int indexPosition) {
-    int value = table.getValue(indexPosition);
-    Serial.print(indexPosition);
-    Serial.print("\t");
-    Serial.println(value);
-}
-
-void modify() {
-  String  inputKey = "modifyKey";
-  int newValue = 150;
-  table.setKey(inputKey);
-  Serial.println();
-  table.setValue(newValue);
-}
-
-void printAll() {
-  String  inputKey = "inputKey";
-  int tableSize = table.getSize();
-  String key = table.getKey(inputKey);
-  int value = table.getValue();
-  
-  Serial.println("Size\t Key \t Value");
-  Serial.print(tableSize);
-  Serial.print("\t");
-  Serial.print(key);
-  Serial.print("\t");
-  Serial.println(value);
+  Serial.println("--");
+  //Reset value
+   hashMap[1]("b", 100);
+   hashMap.debug();
+   
+   
+   Serial.println("--");
+   //Index not exist
+   int index = hashMap.getIndexOf("someValue");
+   Serial.print("index of someValue ");
+   Serial.println(index);
+   boolean containsKey = hashMap.containsKey("someValue");
+   Serial.print("Contains someValue ?");
+   Serial.println(containsKey);
+   containsKey = hashMap.containsKey(VALUE_A);
+   Serial.print("Contains VALUE_A ?");
+   Serial.println(containsKey);
 }
 
 void loop() {
+  int index = hashMap.getIndexOf(VALUE_A);
+  int value = hashMap.getValueOf(VALUE_A);
+  
+  for(int i = 0; i < 10; i++) {
+    hashMap[index](VALUE_A, ++value);
+    Serial.println( hashMap.getValueOf(VALUE_A) );
+  }
+  
+  Serial.print("freeMemory()=");
+  Serial.println(freeMemory());
+  delay(1000);
 }
 
 /** Some cool pointer code
